453a4e2d5720c6bdedc8acbbd1236a5f3e52debc
kirill-slavutin <62643015+kirill-slavutin@users.noreply.github.com>
2020-03-26 17:01:08 +0300

    update calculator

--- Calculator.py
+++ Calculator.py
@@ -1,19 +1,26 @@
 number_1 = float(input())
 number_2 = float(input())
 operation = input()
-if number_2 == 0:
-    print("Деление на 0!")
-elif operation == '+':
+if operation == '+':
     print(number_1 + number_2)
 elif operation == '-':
     print(number_1 - number_2)
 elif operation == '/':
-    print(number_1 / number_2)
+    if number_2 == 0:
+        print("Деление на 0!")
+    else:
+        print(number_1 / number_2)
 elif operation == '*':
     print(number_1 * number_2)
 elif operation == 'mod':
-    print(number_1 % number_2)
+    if number_2 == 0:
+        print("Деление на 0!")
+    else:
+        print(number_1 % number_2)
 elif operation == 'pow':
     print(number_1 ** number_2)
 elif operation == 'div':
-    print(number_1 // number_2)
+    if number_2 == 0:
+        print("Деление на 0!")
+    else:
+        print(number_1 // number_2)
